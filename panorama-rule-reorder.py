21b7a2c7742c760c987a3cb46ea5e2fe8527d2ba
Fahad Yousuf <fahadysf@gmail.com>
2021-06-13 19:54:09 +0300

    Panorama interaction capability added.

--- panorama-rule-reorder.py
+++ panorama-rule-reorder.py
@@ -27,14 +27,8 @@ import os
 import sys
 import time
 import getpass
-import logging
-import logging.handlers as lh
+import panoshelpers
 
-try:
-    from panos.firewall import Firewall
-except ImportError:
-    raise ValueError(
-        "pan-os-python module not available, please install it by running 'python3 -m pip install pan-os-python'")
 
 try:
     import yaml
@@ -43,181 +37,44 @@ except ImportError:
         "PyYAML module not available, please install it by running 'python3 -m pip install PyYAML'")
 
 
-def load_config(config_file='config.yml'):
-    import yaml
-    try:
-        with open(config_file, "r") as ymlfile:
-            cfg = yaml.safe_load(ymlfile)
-            return cfg
-    except BaseException as e:
-        print("Config file 'config.yml' not found or couldn't be opened.")
-        exit(1)
-
-
-def get_config_param(dictpath, param):
-    if not param in dictpath.keys():
-        return None
-    else:
-        return dictpath[param]
-    return None
-
-
-# Setup logging
-try:
-    cfgdict = load_config()
-except BaseException as e:
-    app_log.error(
-        "Unable to load configuration. Please ensure config.yml exists and has no errors")
-
-# Setup logging
-if not os.path.exists(cfgdict['log_path']):
-    os.mkdir(cfgdict['log_path'])
-log_file = os.path.join(cfgdict['log_path'], 'ngfw-control-script.log')
-log_rotation_size = 10 * 1024 * 1024  # Size in bytes (This is 10 MB)
-log_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
-
-file_handler = lh.RotatingFileHandler(
-    log_file, maxBytes=log_rotation_size, backupCount=3)
-file_handler.setFormatter(log_formatter)
-file_handler.setLevel(logging.INFO)
-
-console_handler = logging.StreamHandler()
-console_handler.setFormatter(log_formatter)
-console_handler.setLevel(logging.DEBUG)
-
-app_log = logging.getLogger('root')
-if '-debug' in sys.argv:
-    app_log.setLevel(logging.DEBUG)
-else:
-    app_log.setLevel(logging.INFO)
-app_log.addHandler(file_handler)
-app_log.addHandler(console_handler)
-
-
-def save_config(cfgdict, config_file='config.yml'):
-    with open(config_file, 'w') as outfile:
-        yaml.dump(cfgdict, outfile, default_flow_style=False)
-    return
-
-
-def get_credentials(fw_addr):
-    app_log.info(
-        f"API Key for firewall {fw_addr} is not defined in config.yml")
-    print("Please enter the username and password for the user for the API access.")
-    print("Configuration file (config.yml) will be updated automatically once the API Key is created.")
-    username = input("Enter username: ")
-    password = getpass.getpass()
-    return username, password
-
-
-def gen_api_key(fw_addr, username='', password=''):
-    if not (len(username) or len(password)):
-        username, password = get_credentials(fw_addr)
-        try:
-            fw_obj = Firewall(fw_addr,
-                              api_username=username,
-                              api_password=password)
-            if fw_obj.api_key:
-                app_log.info(
-                    f"API Key generated for firewall {fw_addr} for username {username}")
-                return fw_obj, fw_obj.api_key
-        except BaseException as e:
-            app_log.error("Error generating API Key")
-            app_log.exception(f"Got exception in gen_api_key: {e.message}")
-            exit(1)
-
-
-def initialize_fw_objs(cfgdict):
-    """[summary]
-
-    Arguments:
-        cfgdict {[type]} -- [description]
-
-    Returns:
-        [type] -- [description]
-    """
-    fw_dict = cfgdict['firewalls']
-    fw_objs = list()
-
-    # Check for API Keys in Config
-    config_dirty = 0
-    for fw in fw_dict.keys():
-        app_log.info(f"Connecting to firewall {fw}")
-        try:
-            if ('api_key' not in fw_dict[fw].keys()) or (fw_dict[fw]['api_key'] == None) or (fw_dict[fw]['api_key'] == ''):
-                fw_obj, api_key = gen_api_key(fw)
-                fw_objs.append(fw_obj)
-                fw_dict[fw]['api_key'] = api_key
-                config_dirty = 1
-            else:
-                fw_obj = Firewall(
-                    fw, api_key=fw_dict[fw]['api_key'], timeout=5)
-
-            if get_config_param(fw_dict[fw], 'ha_peer_ip') != None:
-                fw2 = fw_dict[fw]['ha_peer_ip']
-                fw_obj_ha = Firewall(fw2, api_key=fw_dict[fw]['api_key'])
-                fw_obj.set_ha_peers(fw_obj_ha)
-
-            fw_objs.append(fw_obj)
-
-            if config_dirty:
-                cfgdict['firewalls'] = fw_dict
-                save_config(cfgdict)
-        except BaseException as e:
-            app_log.error(
-                f"Could not initialize conneciton to {fw}: {e.message}")
-
-    return fw_objs
-
-    """[summary] Resets the GP Satellite connection
-    Arguments:
-        fw_obj {pan} -- [description]
-        gp_gateway {str} -- [description] IP address of GP Gateway as string
-        gp_satellite_name {str} -- [description] GP Satellite Name
-    """
-    cmd_xml_str = f'<test><global-protect-satellite><gateway-reconnect><satellite>{gp_satellite_name}</satellite><gateway-address>{gp_gateway}</gateway-address><method>activation</method></gateway-reconnect></global-protect-satellite></test>'
-
-    try:
-        result = fw_obj.op(cmd_xml_str, cmd_xml=False, xml=True)
-    except BaseException:
-        raise
-    return result.decode('utf-8')
-
-
 def main():
     """This is a generic example of cycling through HA firewalls and doing something via API.
 
     Returns:
         [type]: [description]
     """
-    # Cycle through the firewalls defined in the config.
-    fw_objs = initialize_fw_objs(cfgdict)
-    for fw in fw_objs:
-        if fw.ha_peer != None:
-            fw.refresh_ha_active()
-            fw_active = fw.active()
-            cfgdict['firewalls'][fw_active.hostname] = cfgdict['firewalls'][fw.hostname]
+    # Cycle through the Panoramas defined in the config.
+    panorama_objs = panoshelpers.initialize_panorama_objs(cfgdict)
+    for panorama in panorama_objs:
+        if panorama.ha_peer != None:
+            panorama.refresh_ha_active()
+            panorama_active = panorama.active()
+            cfgdict['firewalls'][panorama_active.hostname] = cfgdict['firewalls'][panorama.hostname]
             app_log.info(
-                f"HA enabled on firewall. Active firewall is {fw_active.hostname}")
+                f"HA enabled on firewall. Active firewall is {panorama_active.hostname}")
         else:
-            fw_active = fw
+            panorama_active = panorama
 
         app_log.info(
-            f'Doing something -- UPDATE THIS MESSAGE OBVIOUSLY -- on Firewall {fw.hostname}')
+            f'Doing something -- UPDATE THIS MESSAGE OBVIOUSLY -- on Panorama {panorama.hostname}')
 
         # Execute your logic here.
-
+        data = panorama_active.op("show system info")
+        data_str = panoshelpers.prettify(data)
+        print(data_str)
         # Write more logic here (Pending)
         # --------
         fail_counter = 0
-        # Write some actual logic using the fw_active instance to execute stuff on the active FW.
+        # Write some actual logic using the panorama_active instance to execute stuff on the active panorama.
         # ##
         app_log.info(
-            f'Process completed on firewall {fw_active.hostname}.')
+            f'Process completed on Panorama {panorama_active.hostname}.')
     return True
 
 
 if __name__ == '__main__':
+    app_log = panoshelpers.app_log
+    cfgdict = panoshelpers.cfgdict
     if 'daemon_mode' in cfgdict.keys() and cfgdict['daemon_mode']:
         try:
             while True:
---
+++ panoshelpers.py
@@ -0,0 +1,204 @@
+import logging
+import os
+import sys
+import logging
+import logging.handlers as lh
+import xml.dom.minidom as minidom
+from xml.etree import ElementTree
+
+try:
+    from panos.firewall import Firewall
+    from panos.panorama import Panorama
+except ImportError:
+    raise ValueError(
+        "pan-os-python module not available, please install it by running 'python3 -m pip install pan-os-python'")
+
+
+def load_config(config_file='config.yml'):
+    import yaml
+    try:
+        with open(config_file, "r") as ymlfile:
+            cfg = yaml.safe_load(ymlfile)
+            return cfg
+    except BaseException as e:
+        print("Config file 'config.yml' not found or couldn't be opened.")
+        raise
+        exit(1)
+
+
+def get_config_param(dictpath, param):
+    if not param in dictpath.keys():
+        return None
+    else:
+        return dictpath[param]
+    return None
+
+
+def save_config(cfgdict, config_file='config.yml'):
+    with open(config_file, 'w') as outfile:
+        yaml.dump(cfgdict, outfile, default_flow_style=False)
+    return
+
+
+def get_credentials(panorama_addr):
+    app_log.info(
+        f"API Key for firewall {panorama_addr} is not defined in config.yml")
+    print("Please enter the username and password for the user for the API access.")
+    print("Configuration file (config.yml) will be updated automatically once the API Key is created.")
+    username = input("Enter username: ")
+    password = getpass.getpass()
+    return username, password
+
+
+def gen_api_key(panorama_addr, username='', password=''):
+    if not (len(username) or len(password)):
+        username, password = get_credentials(panorama_addr)
+        try:
+            panorama_obj = Firewall(panorama_addr,
+                                    api_username=username,
+                                    api_password=password)
+            if panorama_obj.api_key:
+                app_log.info(
+                    f"API Key generated for firewall {panorama_addr} for username {username}")
+                return panorama_obj, panorama_obj.api_key
+        except BaseException as e:
+            app_log.error("Error generating API Key")
+            app_log.exception(f"Got exception in gen_api_key: {e.message}")
+            exit(1)
+
+
+def initialize_fw_objs(cfgdict):
+    """[summary]
+
+    Arguments:
+        cfgdict {[type]} -- [description]
+
+    Returns:
+        [type] -- [description]
+    """
+    if 'firewalls' in cfgdict.keys():
+        fw_dict = cfgdict['firewalls']
+    else:
+        fw_dict = {}
+    fw_objs = list()
+
+    # Check for API Keys in Config
+    config_dirty = 0
+    for fw in fw_dict.keys():
+        app_log.info(f"Connecting to firewall {fw}")
+        try:
+            if ('api_key' not in fw_dict[fw].keys()) or (fw_dict[fw]['api_key'] == None) or (fw_dict[fw]['api_key'] == ''):
+                fw_obj, api_key = gen_api_key(fw)
+                fw_objs.append(fw_obj)
+                fw_dict[fw]['api_key'] = api_key
+                config_dirty = 1
+            else:
+                fw_obj = Firewall(
+                    fw, api_key=fw_dict[fw]['api_key'], timeout=5)
+
+            if get_config_param(fw_dict[fw], 'ha_peer_ip') != None:
+                fw2 = fw_dict[fw]['ha_peer_ip']
+                fw_obj_ha = Firewall(
+                    fw2, api_key=fw_dict[fw]['api_key'])
+                fw_obj.set_ha_peers(fw_obj_ha)
+
+            fw_objs.append(fw_obj)
+
+            if config_dirty:
+                cfgdict['firewalls'] = fw_dict
+                save_config(cfgdict)
+        except BaseException as e:
+            app_log.error(
+                f"Could not initialize conneciton to {fw}: {e.message}")
+
+    return fw_objs
+
+
+def initialize_panorama_objs(cfgdict):
+    """[summary]
+
+    Arguments:
+        cfgdict {[type]} -- [description]
+
+    Returns:
+        [type] -- [description]
+    """
+    if 'panoramas' in cfgdict.keys():
+        panorama_dict = cfgdict['panoramas']
+    else:
+        panorama_dict = {}
+    panorama_objs = list()
+
+    # Check for API Keys in Config
+    config_dirty = 0
+    for panorama in panorama_dict.keys():
+        app_log.info(f"Connecting to Panorama {panorama}")
+        try:
+            if ('api_key' not in panorama_dict[panorama].keys()) or (panorama_dict[panorama]['api_key'] == None) or (panorama_dict[panorama]['api_key'] == ''):
+                panorama_obj, api_key = gen_api_key(panorama)
+                panorama_objs.append(panorama_obj)
+                panorama_dict[panorama]['api_key'] = api_key
+                config_dirty = 1
+            else:
+                panorama_obj = Firewall(
+                    panorama, api_key=panorama_dict[panorama]['api_key'], timeout=5)
+
+            if get_config_param(panorama_dict[panorama], 'ha_peer_ip') != None:
+                panorama2 = panorama_dict[panorama]['ha_peer_ip']
+                panorama_obj_ha = Panorama(
+                    panorama2, api_key=panorama_dict[panorama]['api_key'])
+                panorama_obj.set_ha_peers(panorama_obj_ha)
+
+            panorama_objs.append(panorama_obj)
+
+            if config_dirty:
+                cfgdict['panoramas'] = panorama_dict
+                save_config(cfgdict)
+        except BaseException as e:
+            app_log.error(
+                f"Could not initialize conneciton to {panorama}: {e.message}")
+
+    return panorama_objs
+
+
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ElementTree.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    data_str = reparsed.toprettyxml(indent="\t")
+    data_str = os.linesep.join([s for s in data_str.splitlines() if s.strip()])
+    return data_str
+
+
+# Read Config
+app_log = logging.getLogger('root')
+if '-debug' in sys.argv:
+    app_log.setLevel(logging.DEBUG)
+else:
+    app_log.setLevel(logging.INFO)
+
+try:
+    cfgdict = load_config()
+    if not os.path.exists(cfgdict['log_path']):
+        os.mkdir(cfgdict['log_path'])
+except BaseException as e:
+    app_log.error(
+        "Unable to load configuration. Please ensure config.yml exists and has no errors")
+    raise(e)
+
+# Setup logging
+log_file = os.path.join(cfgdict['log_path'], 'ngfw-control-script.log')
+log_rotation_size = 10 * 1024 * 1024  # Size in bytes (This is 10 MB)
+log_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+
+file_handler = lh.RotatingFileHandler(
+    log_file, maxBytes=log_rotation_size, backupCount=3)
+file_handler.setFormatter(log_formatter)
+file_handler.setLevel(logging.INFO)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(log_formatter)
+console_handler.setLevel(logging.DEBUG)
+app_log.addHandler(file_handler)
+app_log.addHandler(console_handler)
